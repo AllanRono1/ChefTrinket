4f9fe10e93c274b05e794d04d56c1d45b1bc3092
AllanRono1 <allanrono18@gmail.com>
2026-04-09 08:41:56 +0300

    Inital commit. Added a header section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import bear from './assets/bear.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div className='header-img'>
+        </div>
+        <h1>Chef Trinket</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
