2fdcc45d67bf3f192acbdc2185888da35ba3a05a
AllanRono1 <allanrono18@gmail.com>
2026-04-09 09:56:39 +0300

    Extract Header to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import bear from './assets/bear.png'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <header>
-        <div className='header-img'></div>
-        <h1>Chef Trinket</h1>
-      </header>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return(
+        
+        <header>
+            <div className='header-img'></div>
+            <h1>Chef Trinket</h1>
+      </header>
+    )
+}
+
+export default Header
