0cc7d640ec84d04c1618a6fe0e707be70f0a15cd
AllanRono1 <allanrono18@gmail.com>
2026-04-10 14:27:46 +0300

    Added functionality to perform a POST method

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import { useState } from 'react'
+import React from 'react'
 import Header from './components/Header'
 import './App.css'
 
 function App() {
 
+  const [ingredientsList, setIngredientsList] = React.useState(["oregano", "thyme", "seasoning"])
+  const list = ingredientsList.map((item) => {
+    return <li>{item}</li>
+  })
+
+  function addIngredient() {
+    setIngredientsList([...ingredientsList], newItem)
+    setNewItem()
+  }
+
+  function handleClick(event) {
+    event.preventDefault()
+    const formEl = event.currentTarget
+    const formData = new FormData(formEl)
+    const input = formData.get('input')
+    console.log(input)
+    formEl.reset()
+  }
+
   return (
     <>
       <Header />
       <main>
-        <div className='input-container'>
-          <input type="text" />
+        <form onSubmit={handleClick} method="POST" className='input-container'>
+          <input type="text"
+          placeholder='eg. basil'
+          name='input'/>
           <button>Add ingredient</button>
-        </div>
+        </form>
+        <ul>
+          {list}
+        </ul>
       </main>
     </>
   )
